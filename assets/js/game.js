c9cd34f9c6361f202f01dbb80a9cd15d1760ef6a
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-02-08 11:35:12 -0600

    playerName created

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?")
+
+console.log(playerName);
+// This will math and log 20
+console.log(10+10);
+// What is this?
+console.log.apply("Our robot's name is" + playerName)
+
+function fight () {
+window.alert("The fight has begun!");
+}
+
+// fight();
